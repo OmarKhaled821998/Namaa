3aea055eb341a8080fb9e103b52c62718b51846b
OmarKhaled821998 <omarkhaled8991@gmail.com>
2025-07-05 05:10:00 +0300

    Save categories

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,6 +1,6 @@
 import type { CollectionConfig } from 'payload'
 
-export const Users: CollectionConfig = {
+const UsersCollection: CollectionConfig = {
   slug: 'users',
   admin: {
     useAsTitle: 'email',
@@ -11,5 +11,4 @@ export const Users: CollectionConfig = {
     // Add more fields as needed
   ],
 };
-export default Users; // Make sure it's exported!
-
+export default UsersCollection;
